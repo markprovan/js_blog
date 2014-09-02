57260a23ae87b1b8d6d8e3521bc32012d69226d4
Mark Provan <markgprovan@gmail.com>
2014-09-02 18:26:52 +0100

    Added author

--- app/models/post.js
+++ app/models/post.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr("string"),
   body: DS.attr("string"),
+  author: DS.belongsTo("author", {async: true}),
 
   bodyAsMarkdown: function() {
     if (!Ember.isNone(this.get("body"))) {
--- app/routes/post.js
+++ app/routes/post.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    console.log(params);
     return this.get("store").getById("post", params.id);
   }
 });
